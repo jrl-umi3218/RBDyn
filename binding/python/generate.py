9e7c078864d7201bc4d4bd0122b3fb7cef437756
Joris Vaillant <joris.vaillant@gmail.com>
2012-04-02 10:57:44 +0200

    Fix bad licence copy/past.

--- binding/python/generate.py
+++ binding/python/generate.py
@@ -1,17 +1,17 @@
-# This file is part of SpaceVecAlg.
+# This file is part of RBDyn.
 #
-# SpaceVecAlg is free software: you can redistribute it and/or modify
+# RBDyn is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# SpaceVecAlg is distributed in the hope that it will be useful,
+# RBDyn is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with SpaceVecAlg.  If not, see <http://www.gnu.org/licenses/>.
+# along with RBDyn.  If not, see <http://www.gnu.org/licenses/>.
 
 from pybindgen import *
 import sys
