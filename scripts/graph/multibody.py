65efbae837798bb66424f38f8e7583d4b776e6cd
Joris Vaillant <joris.vaillant@gmail.com>
2012-04-04 11:56:52 +0200

    Add legs multibody, fix rendering.

--- scripts/graph/multibody.py
+++ scripts/graph/multibody.py
@@ -3,10 +3,10 @@ import numpy as np
 from tvtk.tools import ivtk
 from tvtk.api import tvtk
 
-def show_multibody(mb, mbc):
-  bG = [i for i in mbc.bodyGlobal]
+def show_multibody(mb, mbc, v=None):
+  bG = [i for i in mbc.bodyPosW]
 
-  v = ivtk.IVTKWithCrustAndBrowser(size=(600,600))
+  v = v if v is not None else ivtk.IVTKWithCrustAndBrowser(size=(600,600))
   v.open()
 
   for i in range(mb.nrBodies()):
@@ -17,16 +17,46 @@ def show_multibody(mb, mbc):
       Xp = bG[mb.parent(i)]
       XpT = Xp.translation()
 
+      Xpj = mb.transformFrom(i)*Xp
+      Xij = mb.transformTo(i).inv()*Xi
+
+      XpjT = Xpj.translation()
+      XijT = Xij.translation()
+
+      # show link from parent body to joint
       ls = tvtk.LineSource(point1=(XpT[0], XpT[1], XpT[2]),
+                           point2=(XpjT[0], XpjT[1], XpjT[2]))
+      lm = tvtk.PolyDataMapper(input=ls.output)
+      la = tvtk.Actor(mapper=lm)
+      v.scene.add_actors(la)
+
+      # show link from joint to body
+      ls = tvtk.LineSource(point1=(XijT[0], XijT[1], XijT[2]),
                            point2=(XiT[0], XiT[1], XiT[2]))
       lm = tvtk.PolyDataMapper(input=ls.output)
       la = tvtk.Actor(mapper=lm)
       v.scene.add_actors(la)
 
-    ss = tvtk.SphereSource(center=(XiT[0], XiT[1], XiT[2]),
-                           radius=0.1)
-    sm = tvtk.PolyDataMapper(input=ss.output)
-    sa = tvtk.Actor(mapper=sm)
+      # show joint
+      ss = tvtk.SphereSource(center=(XijT[0], XijT[1], XijT[2]),
+                             radius=0.05)
+      sm = tvtk.PolyDataMapper(input=ss.output)
+      sa = tvtk.Actor(mapper=sm)
+      v.scene.add_actors(sa)
 
-    v.scene.add_actors(sa)
+
+    #ss = tvtk.SphereSource(center=(XiT[0], XiT[1], XiT[2]),
+    #                       radius=0.1)
+    #sm = tvtk.PolyDataMapper(input=ss.output)
+    #sa = tvtk.Actor(mapper=sm)
+
+    #v.scene.add_actors(sa)
+    a = tvtk.Axes()
+    a.scale_factor = 0.1
+    #a.origin = np.array([XiT[0], XiT[1], XiT[2]])
+    am = tvtk.PolyDataMapper(input=a.output)
+    aa = tvtk.Actor(mapper=am)
+    v.scene.add_actors(aa)
+
+  return v
 
--- scripts/robots/test_bots.py
+++ scripts/robots/test_bots.py
@@ -2,6 +2,48 @@ from eigen3 import Vector3d, Matrix3d
 import spacevecalg as sva
 import rbdyn as rbd
 
+def make_leg():
+  rbi = sva.RBInertia(1., Vector3d.Zero(), Matrix3d.Identity())
+
+  b0 = rbd.Body(rbi, 0, 'b0')
+  b1 = rbd.Body(rbi, 1, 'b1')
+  b2 = rbd.Body(rbi, 2, 'b2')
+  b3 = rbd.Body(rbi, 3, 'b3')
+
+  j0 = rbd.Joint(rbd.Joint.Spherical, True, 0, 'j0')
+  j1 = rbd.Joint(rbd.Joint.Spherical, True, 1, 'j1')
+  j2 = rbd.Joint(rbd.Joint.Spherical, True, 2, 'j2')
+
+  mbg = rbd.MultiBodyGraph()
+
+  mbg.addBody(b0)
+  mbg.addBody(b1)
+  mbg.addBody(b2)
+  mbg.addBody(b3)
+
+  mbg.addJoint(j0)
+  mbg.addJoint(j1)
+  mbg.addJoint(j2)
+
+  mbg.linkBodies(0, sva.PTransform(Vector3d(0., -0.15, -0.1)),
+                 1, sva.PTransform.Identity(), 0)
+  mbg.linkBodies(1, sva.PTransform(Vector3d(0.025, -0.35, 0.)),
+                 2, sva.PTransform.Identity(), 1)
+  mbg.linkBodies(2, sva.PTransform(Vector3d(0.025, -0.375, 0.)),
+                 3, sva.PTransform.Identity(), 2)
+
+  mb = mbg.makeMultiBody(0, False);
+
+  mbc = rbd.MultiBodyConfig(mb)
+  mbc.q = [[1., 0., 0., 0., 0., 0., 0.],
+           [1., 0., 0., 0.],
+           [1., 0., 0., 0.],
+           [1., 0., 0., 0.]]
+
+  return (mb, mbc, mbg)
+
+
+
 def make_XYZ_ARM():
   rbi = sva.RBInertia(1., Vector3d.Zero(), Matrix3d.Identity())
 
