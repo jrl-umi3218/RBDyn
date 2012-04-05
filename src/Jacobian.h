90f12265ef620d16f1c95327dfb0a06cb6d3c25b
Joris Vaillant <joris.vaillant@gmail.com>
2012-04-05 11:38:01 +0200

    Add a guard to Jacobian header file.

--- src/Jacobian.h
+++ src/Jacobian.h
@@ -13,6 +13,8 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with RBDyn.  If not, see <http://www.gnu.org/licenses/>.
 
+#pragma once
+
 // includes
 // RBDyn
 #include "MultiBody.h"
