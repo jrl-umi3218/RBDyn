9738db7c25db0b3e4e19db2425cd1c20593d3b2c
Joris Vaillant <joris.vaillant@gmail.com>
2012-04-10 17:58:25 +0200

    Add GraphicMultiBody python class to improve robot renderer.

--- scripts/graph/multibody.py
+++ scripts/graph/multibody.py
@@ -3,6 +3,68 @@ import numpy as np
 from tvtk.tools import ivtk
 from tvtk.api import tvtk
 
+class GraphicMultiBody:
+  def __init__(self, mb, v=None):
+    self.bodyS = []
+    self.bodyA = []
+
+    self.jointS = []
+    self.jointA = []
+
+    self.viewer = v if v is not None else ivtk.IVTKWithCrustAndBrowser(size=(600,600))
+    self.viewer.open()
+
+    for i in range(mb.nrBodies()):
+      ls = tvtk.LineSource()
+      lm = tvtk.PolyDataMapper(input=ls.output)
+      la = tvtk.Actor(mapper=lm)
+      la.visibility = 0
+      self.viewer.scene.add_actors(la)
+
+      self.bodyS.append(ls)
+      self.bodyA.append(la)
+
+      ss = tvtk.SphereSource(radius=0.05)
+      sm = tvtk.PolyDataMapper(input=ss.output)
+      sa = tvtk.Actor(mapper=sm)
+      sa.visibility = 0
+      self.viewer.scene.add_actors(sa)
+
+      self.jointS.append(ss)
+      self.jointA.append(sa)
+
+
+  def draw(self, mb, mbc):
+    bG = list(mbc.bodyPosW)
+
+    for i in range(mb.nrBodies()):
+      Xi = bG[i]
+      XiT = Xi.translation()
+
+      if mb.parent(i) != -1:
+        Xp = bG[mb.parent(i)]
+        XpT = Xp.translation()
+
+        # show link from body base to joint
+        ls = self.bodyS[i]
+        ls.point1 = (XpT[0], XpT[1], XpT[2])
+        ls.point2 = (XiT[0], XiT[1], XiT[2])
+
+        la = self.bodyA[i]
+        la.visibility = 1
+
+        # show joint
+        ss = self.jointS[i]
+        ss.center=(XiT[0], XiT[1], XiT[2])
+
+        sa = self.jointA[i]
+        sa.visibility = 1
+
+    self.viewer.scene.render()
+
+
+
+
 def show_multibody(mb, mbc, v=None):
   bG = [i for i in mbc.bodyPosW]
 
