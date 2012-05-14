ee1e3d3fc834399c513ff714488b5b2eda59d353
Joris Vaillant <joris.vaillant@gmail.com>
2012-05-14 18:04:37 +0200

    Improve multibody displaying.

--- scripts/graph/multibody.py
+++ scripts/graph/multibody.py
@@ -1,16 +1,34 @@
 import math
 import numpy as np
 
-from eigen3 import toNumpy, toEigen
+from eigen3 import toNumpy, toEigen, Quaterniond, Vector3d
 import spacevecalg as sva
 
 from mayavi import mlab
 from tvtk.api import tvtk
 
+
+
+def setTransform(actor, transform):
+  R = transform.rotation()
+  T = transform.translation()
+  actor.user_transform.set_matrix((R[0], R[1], R[2], T[0],
+                                   R[3], R[4], R[5], T[1],
+                                   R[6], R[7], R[8], T[2],
+                                     0.,   0.,   0.,   1.))
+
+  #actor.user_transform.set_matrix((R[0], R[3], R[6], T[0],
+  #                                 R[1], R[4], R[7], T[1],
+  #                                 R[2], R[5], R[8], T[2],
+  #                                   0.,   0.,   0.,   1.))
+
+
+
 class GraphicMultiBody:
   def __init__(self, mb):
     self.bodyS = []
     self.bodyA = []
+    self.bodyT = []
 
     self.comS = []
     self.comA = []
@@ -26,25 +44,64 @@ class GraphicMultiBody:
     self.transformVel = [(0., 0., 0., 0.)]*mb.nrJoints()
 
     for i in range(mb.nrBodies()):
-      ls = tvtk.LineSource()
-      lm = tvtk.PolyDataMapper(input=ls.output)
-      la = tvtk.Actor(mapper=lm)
-      la.visibility = 0
-      self.viewer.scene.add_actors(la)
+      cs = tvtk.CylinderSource()
 
-      self.bodyS.append(ls)
-      self.bodyA.append(la)
+      cs.height = np.linalg.norm(toNumpy(mb.transform(i).translation()))
+      cs.radius = 0.01
+      cs.center = (0., cs.height/2., 0.)
 
-      coms = tvtk.CubeSource(x_length=0.25, y_length=0.25, z_length=0.25)
+      quat = Quaterniond()
+      if np.linalg.norm(toNumpy(mb.transform(i).translation())) == 0.:
+        quat.setIdentity()
+      else:
+        quat.setFromTwoVectors(mb.transform(i).translation(), Vector3d.UnitY())
+
+      cm = tvtk.PolyDataMapper(input=cs.output)
+      ca = tvtk.Actor(mapper=cm)
+      ca.property.color = (0., 1., 1.)
+      ca.user_transform = tvtk.Transform()
+      ca.visibility = 0
+      self.viewer.scene.add_actors(ca)
+
+      self.bodyS.append(cs)
+      self.bodyA.append(ca)
+      self.bodyT.append(sva.PTransform(quat.matrix()))
+
+
+
+      comPos = toNumpy(mb.body(i).inertia().momentum())/mb.body(i).inertia().mass()
+      lBody = np.minimum(np.zeros(3).T, comPos)
+      uBody = np.maximum(np.zeros(3).T, comPos)
+
+      for j in range(mb.nrJoints()):
+        if mb.parent(j) == i:
+          t = toNumpy(mb.transform(j).translation())
+          lBody = np.minimum(lBody, t)
+          uBody = np.maximum(uBody, t)
+
+      sBody = uBody + lBody
+      oBody = uBody - lBody
+
+      oBody[oBody < 0.1] = 0.1*mb.body(i).inertia().mass()
+
+      coms = tvtk.CubeSource(x_length=oBody[0,0], y_length=oBody[1,0], z_length=oBody[2,0])
+      coms.center = (sBody[0,0]/2., sBody[1,0]/2., sBody[2,0]/2.)
+
+
       comm = tvtk.PolyDataMapper(input=coms.output)
       coma = tvtk.Actor(mapper=comm)
+      coma.property.color = (0.501, 0., 0.501)
+      coma.property.opacity = 0.2
+      coma.user_transform = tvtk.Transform()
       coma.visibility = 0
       self.viewer.scene.add_actors(coma)
 
       self.comS.append(coms)
       self.comA.append(coma)
 
-      ss = tvtk.SphereSource(radius=0.05)
+
+
+      ss = tvtk.SphereSource(radius=0.02)
       sm = tvtk.PolyDataMapper(input=ss.output)
       sa = tvtk.Actor(mapper=sm)
       sa.visibility = 0
@@ -52,6 +109,8 @@ class GraphicMultiBody:
 
       self.jointS.append(ss)
       self.jointA.append(sa)
+
+
 
       arrs = tvtk.ArrowSource()
       arrm = tvtk.PolyDataMapper(input=arrs.output)
@@ -79,31 +138,25 @@ class GraphicMultiBody:
 
       com = toNumpy(mb.body(i).inertia().momentum())/mb.body(i).inertia().mass()
       com = toEigen(com)
+      com = Vector3d.Zero()
 
       comPos = sva.PTransform(com)*Xi
-      comT = comPos.translation()
 
       comA = self.comA[i]
-      comA.position = (comT[0], comT[1], comT[2])
+      setTransform(comA, comPos)
       comA.visibility = 1
 
       if mb.parent(i) != -1:
         Xp = bG[mb.parent(i)]
-        XpT = Xp.translation()
 
         # show link from body base to joint
-        ls = self.bodyS[i]
-        ls.point1 = (XpT[0], XpT[1], XpT[2])
-        ls.point2 = (XiT[0], XiT[1], XiT[2])
-
         la = self.bodyA[i]
+        setTransform(la, self.bodyT[i]*Xp)
         la.visibility = 1
 
         # show joint
-        ss = self.jointS[i]
-        ss.center=(XiT[0], XiT[1], XiT[2])
-
         sa = self.jointA[i]
+        sa.position = (XiT[0], XiT[1], XiT[2])
         sa.visibility = 1
 
 
