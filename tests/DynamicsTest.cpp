23c4073e1e1eff6eb7a58b7695305d2d27806936
Joris Vaillant <joris.vaillant@gmail.com>
2012-11-14 15:33:52 +0100

    Add unit test for freeflyer dynamics.

--- tests/DynamicsTest.cpp
+++ tests/DynamicsTest.cpp
@@ -120,12 +120,28 @@ void makeRandomVecVec(std::vector<std::vector<double>>& vec)
 			v2 = EScalar::Random()(0)*10.;
 }
 
+void normalizeQuat(std::vector<double>& q)
+{
+	Eigen::Vector4d qv(q[0], q[1], q[2], q[3]);
+	double norm = qv.norm();
+	for(int i = 0; i < 4; ++i)
+		q[i] /= norm;
+}
+
 void makeRandomConfig(rbd::MultiBodyConfig& mbc)
 {
 	makeRandomVecVec(mbc.q);
 	makeRandomVecVec(mbc.alpha);
 	makeRandomVecVec(mbc.alphaD);
 	makeRandomVecVec(mbc.jointTorque);
+
+	for(std::size_t i = 0; i < mbc.q.size(); ++i)
+	{
+		if(mbc.q[i].size() == 4 || mbc.q[i].size() == 7)
+		{
+			normalizeQuat(mbc.q[i]);
+		}
+	}
 }
 
 Eigen::MatrixXd makeHFromID(const rbd::MultiBody& mb,
@@ -178,7 +194,9 @@ Eigen::MatrixXd makeHFromID(const rbd::MultiBody& mb,
 	return H;
 }
 
-BOOST_AUTO_TEST_CASE(IDvsFD)
+
+
+BOOST_AUTO_TEST_CASE(IDvsFDFixed)
 {
 	using namespace Eigen;
 	using namespace sva;
@@ -285,7 +303,167 @@ BOOST_AUTO_TEST_CASE(IDvsFD)
 	fd.forwardDynamics(mb, mbc);
 	MatrixXd ID_H = makeHFromID(mb, mbc, id, fd.C());
 
+	BOOST_CHECK_SMALL((fd.H() - ID_H).norm(), 1e-10);
 
+
+	// check symmetry
+
+	MatrixXd L = fd.H().triangularView<Lower>();
+	MatrixXd U = fd.H().triangularView<Upper>().transpose();
+
+	BOOST_CHECK_SMALL((L - U).norm(), 1e-10);
+
+
+	// check torque and acceleration output
+
+	// torque -> FD -> alphaD -> ID -> torque
+	makeRandomConfig(mbc);
+
+	VectorXd vT1(mb.nrDof()), vT2(mb.nrDof());
+
+	forwardKinematics(mb, mbc);
+	forwardVelocity(mb, mbc);
+
+	paramToVector(mbc.jointTorque, vT1);
+
+	fd.forwardDynamics(mb, mbc);
+	id.inverseDynamics(mb, mbc);
+
+	paramToVector(mbc.jointTorque, vT2);
+
+	BOOST_CHECK_SMALL((vT1 - vT2).norm(), 1e-10);
+
+
+	// alphaD -> ID -> torque -> FD -> alphaD
+	makeRandomConfig(mbc);
+
+	VectorXd vA1(mb.nrDof()), vA2(mb.nrDof());
+
+	forwardKinematics(mb, mbc);
+	forwardVelocity(mb, mbc);
+
+	paramToVector(mbc.alphaD, vA1);
+
+	id.inverseDynamics(mb, mbc);
+	fd.forwardDynamics(mb, mbc);
+
+	paramToVector(mbc.alphaD, vA2);
+
+	BOOST_CHECK_SMALL((vA1 - vA2).norm(), 1e-10);
+}
+
+
+
+
+BOOST_AUTO_TEST_CASE(IDvsFDFree)
+{
+	using namespace Eigen;
+	using namespace sva;
+	using namespace rbd;
+	namespace cst = boost::math::constants;
+
+	typedef Matrix<double, 1, 1> EScalar;
+
+	RBInertia I0(EScalar::Random()(0)*10., Vector3d::Random()*10.,
+		Matrix3d::Random().triangularView<Lower>());
+	RBInertia I1(EScalar::Random()(0)*10., Vector3d::Random()*10.,
+		Matrix3d::Random().triangularView<Lower>());
+	RBInertia I2(EScalar::Random()(0)*10., Vector3d::Random()*10.,
+		Matrix3d::Random().triangularView<Lower>());
+	RBInertia I3(EScalar::Random()(0)*10., Vector3d::Random()*10.,
+		Matrix3d::Random().triangularView<Lower>());
+
+	Body b0(I0, 0, "b0");
+	Body b1(I1, 1, "b1");
+	Body b2(I2, 2, "b2");
+	Body b3(I3, 3, "b3");
+
+	Joint j0 = Joint(Joint::Spherical, true, 0, "j0");
+	Joint j1 = Joint(Joint::RevX, true, 1, "j1");
+	Joint j2 = Joint(Joint::RevZ, true, 2, "j2");
+
+
+	MultiBodyGraph mbg;
+
+	mbg.addBody(b0);
+	mbg.addBody(b1);
+	mbg.addBody(b2);
+	mbg.addBody(b3);
+
+	mbg.addJoint(j0);
+	mbg.addJoint(j1);
+	mbg.addJoint(j2);
+
+	mbg.linkBodies(0, PTransform(Vector3d(0., 0.5, 0.)),
+								 1, PTransform(Vector3d(0., -0.5, 0.)), 0);
+	mbg.linkBodies(1, PTransform(Vector3d(0.5, 0., 0.)),
+								 2, PTransform(Vector3d(0., 0., 0.)), 1);
+	mbg.linkBodies(1, PTransform(Vector3d(-0.5, 0., 0.)),
+								 3, PTransform(Vector3d(0., 0., 0.)), 2);
+
+	MultiBody mb = mbg.makeMultiBody(0, false);
+
+	MultiBodyConfig mbc(mb);
+
+	mbc.q = {{1., 0., 0., 0., 0., 0., 0.}, {1., 0., 0., 0.}, {0.}, {0.}};
+	mbc.alpha = {{0., 0., 0., 0., 0., 0.}, {0., 0., 0.}, {0.}, {0.}};
+	mbc.alphaD = {{0., 0., 0., 0., 0., 0.}, {0., 0., 0.}, {0.}, {0.}};
+	mbc.force = {ForceVec(Vector6d::Zero()),
+							 ForceVec(Vector6d::Zero()),
+							 ForceVec(Vector6d::Zero()),
+							 ForceVec(Vector6d::Zero())};
+
+	forwardKinematics(mb, mbc);
+	forwardVelocity(mb, mbc);
+
+	InverseDynamics id(mb);
+	ForwardDynamics fd(mb);
+
+
+	// check non linear is null
+	mbc.gravity = Vector3d::Zero();
+	fd.computeC(mb, mbc);
+
+	BOOST_CHECK(fd.C().isZero());
+
+
+	// check FD C against ID C
+	mbc.gravity = Vector3d(0., -9.81, 0.);
+	makeRandomConfig(mbc);
+
+	forwardKinematics(mb, mbc);
+	forwardVelocity(mb, mbc);
+
+	fd.computeC(mb, mbc);
+
+	mbc.alphaD = {{0., 0., 0., 0., 0., 0.}, {0., 0., 0.}, {0.}, {0.}};
+	id.inverseDynamics(mb, mbc);
+
+	VectorXd ID_C(mb.nrDof());
+	int dof = 0;
+	for(auto& v1: mbc.jointTorque)
+	{
+		for(auto& v2: v1)
+		{
+			ID_C(dof) = v2;
+			++dof;
+		}
+	}
+
+	BOOST_CHECK_EQUAL(fd.C(), ID_C);
+
+
+	// check FD H against ID H
+	makeRandomConfig(mbc);
+
+	forwardKinematics(mb, mbc);
+	forwardVelocity(mb, mbc);
+
+	fd.forwardDynamics(mb, mbc);
+	MatrixXd ID_H = makeHFromID(mb, mbc, id, fd.C());
+
+	std::cout << fd.H() - ID_H << std::endl;
+	std::cout << std::endl;
 	BOOST_CHECK_SMALL((fd.H() - ID_H).norm(), 1e-10);
 
 
