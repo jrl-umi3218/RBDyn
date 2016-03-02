3156d08b0458522554c6655a20231885774ae9a1
Hervé Audren <h.audren@gmail.com>
2016-03-02 15:24:43 +0900

    Add copyright to IK files

--- src/IK.h
+++ src/IK.h
@@ -1,5 +1,7 @@
 // This file is part of RBDyn.
 //
+// Copyright (C) 2012-2016 CNRS-AIST JRL, CNRS-UM LIRMM
+//
 // RBDyn is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
