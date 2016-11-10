8c6caaae132425c8a64102fac153a2717f149142
Joven Agravante <joven.agravante@gmail.com>
2016-11-10 19:00:44 +0100

    initial merge from mc_non_ros image-constraint branch

--- src/VisServo.cpp
+++ src/VisServo.cpp
@@ -22,28 +22,30 @@
 namespace rbd
 {
 
-Eigen::MatrixXd imagePointJacobian(const Eigen::Vector2d& point2d, const double depthEstimate)
+void imagePointJacobian(const Eigen::Vector2d& point2d, const double depthEstimate, Eigen::Matrix<double, 2, 6>& jac)
 {
-	Eigen::MatrixXd m(2,6);
-	m << point2d[0]*point2d[1], -(1+point2d[0]*point2d[0]),  point2d[1], -1./depthEstimate, 0., point2d[0]/depthEstimate,
-			 1+point2d[1]*point2d[1],   -point2d[0]*point2d[1], -point2d[0], 0., -1./depthEstimate, point2d[1]/depthEstimate;
-	return m;
+	jac << point2d[0]*point2d[1], -(1+point2d[0]*point2d[0]),  point2d[1], -1./depthEstimate, 0., point2d[0]/depthEstimate,
+				 1+point2d[1]*point2d[1],   -point2d[0]*point2d[1], -point2d[0], 0., -1./depthEstimate, point2d[1]/depthEstimate;
 }
 
-Eigen::MatrixXd imagePointJacobian(const Eigen::Vector3d& point3d)
+void imagePointJacobian(const Eigen::Vector3d& point3d, Eigen::Matrix<double, 2, 6>& jac)
 {
-	// normalized image coordinates
-	double x = point3d[0] / point3d[2];
-	double y = point3d[1] / point3d[2];
-	Eigen::MatrixXd m(2,6);
-	m << x*y, -(1+x*x),  y, -1./point3d[2], 0., x/point3d[2],
-			 1+y*y,   -x*y, -x, 0., -1./point3d[2], y/point3d[2];
-	return m;
+	// convert 3D to 2D normalized image coordinates
+	Eigen::Vector2d point2d;
+	point2d << point3d[0] / point3d[2], point3d[1] / point3d[2];
+
+	imagePointJacobian(point2d, point3d[2], jac);
 }
 
-Eigen::MatrixXd poseJacobian(const Eigen::Matrix3d& rotation, const double rot_angle_threshold)
+void imagePointJacobianDot(const Eigen::Vector2d imagePoint, const Eigen::Vector2d imagePointSpeed, const double depth, const double depthDot, Eigen::Matrix<double, 2, 6>& jac)
 {
-	Eigen::MatrixXd m(6,6);
+	double Z_sq = (depth*depth);
+	jac << imagePointSpeed(0)*imagePoint(1) + imagePoint(0)*imagePointSpeed(1), -2*imagePoint(0)*imagePointSpeed(0), imagePointSpeed(1), depthDot/Z_sq, 0., (imagePointSpeed(0)*depth - imagePoint(0)*depthDot)/Z_sq,
+				 2*imagePoint(1)*imagePointSpeed(1), -imagePointSpeed(0)*imagePoint(1)-imagePoint(0)*imagePointSpeed(1), -imagePointSpeed(0), 0., depthDot/Z_sq, (imagePointSpeed(1)*depth - imagePoint(1)*depthDot)/Z_sq;
+}
+
+void poseJacobian(const Eigen::Matrix3d& rotation, Eigen::Matrix<double, 6, 6>& jac, const double rot_angle_threshold)
+{
 	Eigen::Matrix3d i3 = Eigen::Matrix3d::Identity();
 
 	// convert rotation matrix into axis-angle representation
@@ -65,15 +67,17 @@ Eigen::MatrixXd poseJacobian(const Eigen::Matrix3d& rotation, const double rot_a
 		sinc_part = ((std::sin(rot_angle))/rot_angle)/(std::pow(((std::sin(rot_angle/2.))/(rot_angle/2.)),2));
 
 		Eigen::Matrix3d axis_antisym;
-		axis_antisym << 0., -rot_axis(2), rot_axis(1),
-										rot_axis(2), 0., -rot_axis(0),
-										-rot_axis(1), rot_axis(0), 0.;
+		getSkewSym(rot_axis, axis_antisym);
 
 		L_theta_u = i3 - rot_angle*0.5*axis_antisym + (1-(sinc_part))*axis_antisym*axis_antisym;
 	}
-	m << L_theta_u, Eigen::Matrix3d::Zero(),
-			 Eigen::Matrix3d::Zero(), rotation.transpose();
-	return m;
+	jac << L_theta_u, Eigen::Matrix3d::Zero(),
+				 Eigen::Matrix3d::Zero(), rotation.transpose();
+}
+
+void depthDotJacobian(const Eigen::Vector2d imagePointSpeed, const double depthEstimate, Eigen::Matrix<double, 1, 6>& jac)
+{
+	jac << 0., 0., -1., -imagePointSpeed(1)*depthEstimate, imagePointSpeed(0)*depthEstimate, 0.;
 }
 
 void getAngleAxis(const Eigen::Matrix3d& rotation, double& rot_angle, Eigen::Vector3d& rot_axis)
@@ -84,5 +88,13 @@ void getAngleAxis(const Eigen::Matrix3d& rotation, double& rot_angle, Eigen::Vec
 	rot_axis << -rotation(2,1) + rotation(1,2),
 							-rotation(0,2) + rotation(2,0),
 							-rotation(1,0) + rotation(0,1);
+}
+
+
+void getSkewSym(const Eigen::Vector3d& vector, Eigen::Matrix3d& matrix)
+{
+	matrix <<  0.,				-vector(2),  vector(1),
+						 vector(2), 0.,					-vector(0),
+						-vector(1), vector(0),	 0.;
 }
 }
