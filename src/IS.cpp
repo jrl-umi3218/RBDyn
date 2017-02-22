dae1b79f78ad9fc733bcb4794e9b94744f6d84a2
Andrew Hundt <ATHundt@gmail.com>
2017-02-21 23:48:12 -0500

    revert aligned_allocator on vector<MatrixXd> for Eigen < 3.3 https://github.com/jrl-umi3218/Tasks/issues/23

--- src/IS.cpp
+++ src/IS.cpp
@@ -120,7 +120,7 @@ void InverseStatics::inverseStatics(const MultiBody& mb, MultiBodyConfig& mbc)
 
 void InverseStatics::computeTorqueJacobianJoint(
     const MultiBody& mb, MultiBodyConfig& mbc,
-    const std::vector<MatrixXd, Eigen::aligned_allocator<Eigen::MatrixXd> >& jacMomentsAndForces)
+    const std::vector<MatrixXd>& jacMomentsAndForces)
 {
   assert(jacMomentsAndForces.size() == static_cast<size_t>(mb.nrBodies()));
 
@@ -230,7 +230,7 @@ void InverseStatics::computeTorqueJacobianJoint(
 void InverseStatics::computeTorqueJacobianJoint(const MultiBody& mb,
                                                 MultiBodyConfig& mbc)
 {
-  std::vector<MatrixXd, Eigen::aligned_allocator<Eigen::MatrixXd> > jacMandF;
+  std::vector<MatrixXd> jacMandF;
   for (int i = 0; i < mb.nrJoints(); ++i) jacMandF.push_back(MatrixXd(0, 0));
 
   computeTorqueJacobianJoint(mb, mbc, jacMandF);
