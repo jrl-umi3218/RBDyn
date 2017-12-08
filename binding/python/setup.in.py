b6edefef50a7b001c6e25690972362e43fa62b2c
Pierre Gergondet <pierre.gergondet@gmail.com>
2017-12-08 11:14:55 +0900

    No more python-config call in setup.in.py

--- binding/python/setup.in.py
+++ binding/python/setup.in.py
@@ -61,32 +61,11 @@ class pkg_config(object):
     self.library_dirs.append(os.path.dirname(rbdyn_location))
     self.found = True
 
-python_libs = []
-python_lib_dirs = []
-python_others = []
-if not win32_build:
-  tokens = subprocess.check_output(['python-config', '--ldflags']).split()
-  tokens = [ token.decode('ascii') for token in tokens ]
-  for token in tokens:
-    flag = token[:2]
-    value = token[2:]
-    if flag == '-l':
-      python_libs.append(value)
-    elif flag == '-L':
-      python_lib_dirs.append(value)
-    elif token[:1] == '-':
-      python_others.append(token)
-
 config = pkg_config()
 
 config.compile_args.append('-std=c++11')
-for o in python_others:
-  config.compile_args.append(o)
 config.include_dirs.append(os.getcwd() + "/include")
-if not win32_build:
-  config.library_dirs.extend(python_lib_dirs)
-  config.libraries.extend(python_libs)
-else:
+if win32_build:
   config.compile_args.append("-DWIN32")
 
 def GenExtension(name, pkg, ):
