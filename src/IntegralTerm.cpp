18db62cde03464d3db4731940c08695ef94de746
Rafael Cisneros <rafael.cisneros@aist.go.jp>
2018-03-29 15:02:06 +0900

    Removed commented prints and a dirty patch to put P[i] as 0 for some joints

--- src/IntegralTerm.cpp
+++ src/IntegralTerm.cpp
@@ -46,8 +46,6 @@ void IntegralTerm::computeTerm(const rbd::MultiBody& mb,
   {
     Eigen::MatrixXd K;
 
-    //std::cout << "Rafa, in IntegralTerm::computeTerm, fd_->H() =" << std::endl << fd_->H() << std::endl << std::endl;
-
     if (velGainType_ == MassMatrix)
     {
         K = lambda_ * fd_->H();
@@ -72,24 +70,14 @@ void IntegralTerm::computeTerm(const rbd::MultiBody& mb,
     {
         P_ = K * s;
     }
-
-    for (int i = 0; i < mb.nrJoints() + 6; i++)
-      {
-	//if ((i == 16 + 6 || i == 17 + 6) || (i >= 27 + 6 && i <= 35 + 6) || (i >= 45 + 6 && i <= 53 + 6))
-	if (i >= 19 + 6)
-	  P_[i] = 0;
-      }
-
-    std::cout << "Rafa, inside of IntglTerm::computeTerm, P_ = " << P_.transpose() << std::endl << std::endl;
   
-    //gammaD_ = fd_->H().inverse() * P_;
+    // Alternative method to do
+    // gammaD_ = fd_->H().inverse() * P_;
 
-    /*
     gammaD_ = P_;
     L_.compute(fd_->H());
     L_.matrixL().solveInPlace(gammaD_);
     L_.matrixL().transpose().solveInPlace(gammaD_);
-    */
   }
 }
 
