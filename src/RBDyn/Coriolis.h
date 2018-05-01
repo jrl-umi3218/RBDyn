a323ec3fce64a430c4b27f384e21ad4371b88d65
Hervé Audren <herve@ascent.ai>
2018-05-01 16:12:56 +0900

    Fix indentation

--- src/RBDyn/Coriolis.h
+++ src/RBDyn/Coriolis.h
@@ -15,33 +15,33 @@ Eigen::Matrix3d skewSymmetricFromVelocity(const Eigen::Vector3d& v);
 Eigen::Matrix3d rotationMatrixFromVector(const Eigen::Vector3d& angular);
 
 Eigen::MatrixXd expand(const rbd::Jacobian& jac,
-                       const rbd::MultiBody& mb,
-                       const Eigen::MatrixXd& jacMat);
+			const rbd::MultiBody& mb,
+			const Eigen::MatrixXd& jacMat);
 
 void expandAdd(const rbd::Jacobian& jac,
-               const rbd::MultiBody& mb,
-               const Eigen::MatrixXd& jacMat,
-               Eigen::MatrixXd& res);
+		const rbd::MultiBody& mb,
+		const Eigen::MatrixXd& jacMat,
+		Eigen::MatrixXd& res);
 
 Blocks compactPath(const rbd::Jacobian& jac,
-                   const rbd::MultiBody& mb);
+		const rbd::MultiBody& mb);
 
 void compactExpandAdd(const Blocks& compactPath,
-                      const Eigen::MatrixXd& jacMat,
-                      Eigen::MatrixXd& res);
+			const Eigen::MatrixXd& jacMat,
+			Eigen::MatrixXd& res);
 
 class Coriolis
 {
-  public:
-    Coriolis(const rbd::MultiBody& mb);
+	public:
+		Coriolis(const rbd::MultiBody& mb);
 
-    Eigen::MatrixXd coriolis(const rbd::MultiBody& mb, const rbd::MultiBodyConfig& mbc);
+		Eigen::MatrixXd coriolis(const rbd::MultiBody& mb, const rbd::MultiBodyConfig& mbc);
 
-  private:
-    std::vector<rbd::Jacobian> jacs_;
-    std::vector<Eigen::MatrixXd> jacMats_;
-    std::vector<Eigen::MatrixXd> jacDotMats_;
-    std::vector<Blocks> compactPaths_;
+	private:
+		std::vector<rbd::Jacobian> jacs_;
+		std::vector<Eigen::MatrixXd> jacMats_;
+		std::vector<Eigen::MatrixXd> jacDotMats_;
+		std::vector<Blocks> compactPaths_;
 };
 
 } // ns coriolis
--- tests/CoriolisTest.cpp
+++ tests/CoriolisTest.cpp
@@ -4,99 +4,101 @@
 #include <boost/test/unit_test.hpp>
 
 #include <RBDyn/Coriolis.h>
+#include <RBDyn/EulerIntegration.h>
 #include <RBDyn/FD.h>
 #include <RBDyn/FK.h>
 #include <RBDyn/FV.h>
-#include <RBDyn/EulerIntegration.h>
 
 #include <iostream>
 
 BOOST_AUTO_TEST_CASE(CoriolisTest)
 {
-  srand(time(NULL));
+	srand(time(NULL));
 
-  constexpr double TOL = 1e-6;
-  constexpr double BIGTOL = 50*TOL;
+	constexpr double TOL = 1e-6;
+	constexpr double BIGTOL = 50 * TOL;
 
-  rbd::MultiBody mb;
-  rbd::MultiBodyConfig mbc;
-  rbd::MultiBodyGraph mbg;
+	rbd::MultiBody mb;
+	rbd::MultiBodyConfig mbc;
+	rbd::MultiBodyGraph mbg;
 
-  std::tie(mb, mbc, mbg) = makeTree30Dof(false);
+	std::tie(mb, mbc, mbg) = makeTree30Dof(false);
 
-  rbd::MultiBody mbt;
-  rbd::MultiBodyConfig mbtc;
-  rbd::MultiBodyGraph mbtg;
+	rbd::MultiBody mbt;
+	rbd::MultiBodyConfig mbtc;
+	rbd::MultiBodyGraph mbtg;
 
-  std::tie(mbt, mbtc, mbtg) = makeTree30Dof(false);
+	std::tie(mbt, mbtc, mbtg) = makeTree30Dof(false);
 
-  const static int ROUNDS = 1000;
+	const static int ROUNDS = 1000;
 
-  for(int i = 0; i < ROUNDS; ++i)
-  {
-    mbc.zero(mb);
-    mbtc.zero(mbt);
+	for (int i = 0; i < ROUNDS; ++i)
+	{
+		mbc.zero(mb);
+		mbtc.zero(mbt);
 
-    Eigen::VectorXd q = Eigen::VectorXd::Random(mb.nrParams());
-    mbc.q = rbd::sVectorToParam(mb, q);
+		Eigen::VectorXd q = Eigen::VectorXd::Random(mb.nrParams());
+		mbc.q = rbd::sVectorToParam(mb, q);
 
-    for(auto& q : mbc.q)
-    {
-      if(q.size() == 7)
-      {
-        Eigen::Vector3d axis = Eigen::Vector3d::Random();
-        Eigen::AngleAxisd aa(0.5, axis/axis.norm());
-        Eigen::Quaterniond qd(aa);
-        q[0] = qd.w();
-        q[1] = qd.x();
-        q[2] = qd.y();
-        q[3] = qd.z();
-      }
-    }
+		for (auto &q : mbc.q)
+		{
+			if (q.size() == 7)
+			{
+				Eigen::Vector3d axis =
+				    Eigen::Vector3d::Random();
+				Eigen::AngleAxisd aa(0.5, axis / axis.norm());
+				Eigen::Quaterniond qd(aa);
+				q[0] = qd.w();
+				q[1] = qd.x();
+				q[2] = qd.y();
+				q[3] = qd.z();
+			}
+		}
 
-    mbtc.q = mbc.q;
+		mbtc.q = mbc.q;
 
-    rbd::forwardKinematics(mb, mbc);
-    rbd::forwardVelocity(mb, mbc);
+		rbd::forwardKinematics(mb, mbc);
+		rbd::forwardVelocity(mb, mbc);
 
-    rbd::forwardKinematics(mbt, mbtc);
-    rbd::forwardVelocity(mbt, mbtc);
+		rbd::forwardKinematics(mbt, mbtc);
+		rbd::forwardVelocity(mbt, mbtc);
 
-    rbd::ForwardDynamics fd(mbt);
-    fd.computeC(mbt, mbtc);
-    Eigen::VectorXd gravity = fd.C();
+		rbd::ForwardDynamics fd(mbt);
+		fd.computeC(mbt, mbtc);
+		Eigen::VectorXd gravity = fd.C();
 
-    Eigen::VectorXd qd = Eigen::VectorXd::Random(mb.nrDof());
-    mbc.alpha = rbd::sVectorToDof(mb, qd);
-    mbtc.alpha = mbc.alpha;
+		Eigen::VectorXd qd = Eigen::VectorXd::Random(mb.nrDof());
+		mbc.alpha = rbd::sVectorToDof(mb, qd);
+		mbtc.alpha = mbc.alpha;
 
-    rbd::forwardVelocity(mb, mbc);
-    rbd::forwardVelocity(mbt, mbtc);
+		rbd::forwardVelocity(mb, mbc);
+		rbd::forwardVelocity(mbt, mbtc);
 
-    rbd::Coriolis coriolis(mb);
-    Eigen::MatrixXd C = coriolis.coriolis(mb, mbc);
+		rbd::Coriolis coriolis(mb);
+		Eigen::MatrixXd C = coriolis.coriolis(mb, mbc);
 
-    fd.computeC(mbt, mbtc);
-    Eigen::MatrixXd N = fd.C();
+		fd.computeC(mbt, mbtc);
+		Eigen::MatrixXd N = fd.C();
 
-    BOOST_CHECK_SMALL((C*qd + gravity - N).norm(), TOL);
+		BOOST_CHECK_SMALL((C * qd + gravity - N).norm(), TOL);
 
-    fd.computeH(mbt, mbtc);
-    Eigen::MatrixXd m1 = fd.H();
+		fd.computeH(mbt, mbtc);
+		Eigen::MatrixXd m1 = fd.H();
 
-    double dt = 1e-8;
+		double dt = 1e-8;
 
-    rbd::sEulerIntegration(mbt, mbtc, dt);
+		rbd::sEulerIntegration(mbt, mbtc, dt);
 
-    rbd::forwardKinematics(mbt, mbtc);
-    rbd::forwardVelocity(mbt, mbtc);
-    fd.computeH(mbt, mbtc);
+		rbd::forwardKinematics(mbt, mbtc);
+		rbd::forwardVelocity(mbt, mbtc);
+		fd.computeH(mbt, mbtc);
 
-    Eigen::MatrixXd m2 = fd.H();
+		Eigen::MatrixXd m2 = fd.H();
 
-    Eigen::MatrixXd diff = (m2 - m1)/dt - (C+C.transpose());
+		Eigen::MatrixXd diff = (m2 - m1) / dt - (C + C.transpose());
 
-    //Because we are using finite differences, the error is larger on this test
-    BOOST_CHECK_SMALL(diff.norm(), BIGTOL);
-  }
+		// Because we are using finite differences, the error is larger
+		// on this test
+		BOOST_CHECK_SMALL(diff.norm(), BIGTOL);
+	}
 }
