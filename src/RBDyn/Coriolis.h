cef592c4669ef6577e3251433e70e07352fdc851
Hervé Audren <herve@ascent.ai>
2018-05-07 14:46:00 +0900

    Add a bunch of comments

--- src/RBDyn/Coriolis.h
+++ src/RBDyn/Coriolis.h
@@ -13,27 +13,63 @@ namespace rbd
 using Block = std::array<int, 3>;
 using Blocks = std::vector<Block>;
 
+/** Expand a symmetric product of a jacobian by its transpose onto every DoF.
+ * @param jac Jacobian whose product will be expanded
+ * @param mb Multibody system
+ * @param jacMat The product of J^T*J that will be expanded
+ */
 RBDYN_DLLAPI Eigen::MatrixXd expand(const rbd::Jacobian& jac,
 			const rbd::MultiBody& mb,
 			const Eigen::MatrixXd& jacMat);
 
+/** Expand a symmetric product of a jacobian by its transpose onto every DoF
+ * and accumulate the result
+ * @param res Accumulator matrix
+ * @see expand
+ */
 RBDYN_DLLAPI void expandAdd(const rbd::Jacobian& jac,
 		const rbd::MultiBody& mb,
 		const Eigen::MatrixXd& jacMat,
 		Eigen::MatrixXd& res);
 
+/** Compute a compact kinematic path, i.e. the sequence of consecutive blocks
+ * of DoF contained in the original path.
+ * @param jac Jacobian to be compacted
+ * @param mb Multibody system
+ */
 RBDYN_DLLAPI Blocks compactPath(const rbd::Jacobian& jac,
 		const rbd::MultiBody& mb);
 
+/** Expand a symmetric product of a jacobian by its transpose onto every DoF
+ * and accumulate the result using a compact representation of the DoF.
+ * @param compacPath Blocks representing the compact kinematic path of the Jacobian
+ * @param jacMat The matrix containing the product J^T*J to be expanded
+ * @param res The accumulator matrix
+ **/
 RBDYN_DLLAPI void compactExpandAdd(const Blocks& compactPath,
 			const Eigen::MatrixXd& jacMat,
 			Eigen::MatrixXd& res);
 
+/**
+ * Computation of the Coriolis effects matrix on a multibody. The Coriolis
+ * factorization is not unique, we use the formulation of M. Bjerkend and
+ * K. Pettersen in "A new Coriolis matrix factorization", 2012
+ * NB: ForwardDynamics::C() directly computes the product of this matrix with qd.
+ * This C*qd is unique, but C itself is not.
+ */
 class RBDYN_DLLAPI Coriolis
 {
 	public:
+		/** Initialize the required structures
+		 * @param mb Multibody system
+		 */
 		Coriolis(const rbd::MultiBody& mb);
 
+
+		/** Compute the matrix C of Coriolis effects.
+		 * @param mb Multibody system
+		 * @param mbc Multibody configuration associated to mb
+		 */
 		Eigen::MatrixXd coriolis(const rbd::MultiBody& mb, const rbd::MultiBodyConfig& mbc);
 
 	private:
@@ -43,6 +79,6 @@ class RBDYN_DLLAPI Coriolis
 		std::vector<Blocks> compactPaths_;
 };
 
-} // ns coriolis
+} // ns rbd
 
 #endif /* end of include guard: CORIOLIS_H_KED6CAHK */
