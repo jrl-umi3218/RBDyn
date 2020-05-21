d399a4daec8f19b4e1f6ee2fba6e33f2316f2982
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-05-21 22:47:06 +0800

    [actions] Add a conan packaging action

--- conanfile.py
+++ conanfile.py
@@ -6,13 +6,16 @@
 from conans import python_requires, tools
 import os
 
-base = python_requires("Eigen3ToPython/1.0.0@gergondet/stable")
+base = python_requires("Eigen3ToPython/latest@multi-contact/dev")
 
-get_python_version = base.get_python_version
+def get_default_options():
+    out = base.get_default_options()
+    out.update({'*:shared': True})
+    return out
 
 class RBDynConan(base.Eigen3ToPythonConan):
     name = "RBDyn"
-    version = "1.1.0"
+    version = "1.2.1"
     description = "Model the dynamics of rigid body systems"
     topics = ("robotics", "dynamics", "eigen", "python")
     url = "https://github.com/jrl-umi3218/RBDyn"
@@ -20,17 +23,51 @@ class RBDynConan(base.Eigen3ToPythonConan):
     author = "Pierre Gergondet <pierre.gergondet@gmail.com>"
     license = "BSD-2-Clause"
     exports = ["LICENSE"]
-    exports_sources = ["CMakeLists.txt", "conan/CMakeLists.txt", "binding/*", "cmake/*", "doc/*", "src/*"]
-    generators = "cmake"
+    exports_sources = ["CMakeLists.txt", "conan/*", "binding/*", "cmake/*", "doc/*", "src/*"]
+    generators = ["cmake_find_package", "cmake_paths"]
     settings = "os", "arch", "compiler", "build_type"
-    options = { "python_version": ["2.7", "3.3", "3.4", "3.5", "3.6", "3.7"] }
-    default_options = { "python_version": get_python_version() }
+    options = {
+            "python2_version": [None, "2.7"],
+            "python3_version": [None, "3.3", "3.4", "3.5", "3.6", "3.7", "3.8"]
+    }
+    default_options = get_default_options()
 
     requires = (
-        "SpaceVecAlg/1.1.0@gergondet/stable"
+        "SpaceVecAlg/latest@multi-contact/dev",
+        "boost/1.73.0",
+        "yaml-cpp/0.6.3@multi-contact/3rd-party",
+        "tinyxml2/8.0.0@multi-contact/3rd-party"
     )
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
         self.env_info.LD_LIBRARY_PATH.append(os.path.join(self.package_folder, 'lib'))
         self.env_info.PYTHONPATH.append(self._extra_python_path())
+
+    def source(self):
+        base.Eigen3ToPythonConan.source(self)
+        pattern = 'include(CMakeFindDependencyMacro)'
+        replacement = '''if(CONAN_BOOST_ROOT)
+  set(BOOST_ROOT "${{CONAN_BOOST_ROOT}}")
+else()
+  set(BOOST_ROOT "${{PACKAGE_PREFIX_DIR}}")
+endif()
+set(Boost_NO_SYSTEM_PATHS ON)
+list(APPEND CMAKE_MODULE_PATH "${{CMAKE_CURRENT_LIST_DIR}}")
+{}'''.format(pattern)
+        tools.replace_in_file('cmake/Config.cmake.in', pattern, replacement)
+        pattern = 'add_subdirectory(src)'
+        replacement = '''{}
+install(FILES conan/FindBoost.cmake DESTINATION lib/cmake/RBDyn)'''.format(pattern)
+        tools.replace_in_file('CMakeListsOriginal.txt', pattern, replacement)
+        pattern = "Boost::boost"
+        replacement = "$<BUILD_INTERFACE:Boost::Boost>$<INSTALL_INTERFACE:$<IF:$<BOOL:CONAN_BOOST_ROOT>,Boost::Boost,Boost::boost>>"
+        tools.replace_in_file('src/parsers/CMakeLists.txt', pattern, replacement)
+
+    def package(self):
+        cmake = self._configure_cmake()
+        pattern = "BOOL:CONAN_BOOST_ROOT"
+        replacement = "BOOL:${CONAN_BOOST_ROOT}"
+        generated = os.path.join(self.build_folder, 'CMakeFiles', 'Export', 'lib', 'cmake', self.name, '{}Targets.cmake'.format(self.name))
+        tools.replace_in_file(generated, pattern, replacement)
+        cmake.install()
