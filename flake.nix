af60d842b60c929c836eaf808ea5507cd09af016
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-05-03 18:10:00 +0200

    nix(bindings): Overrides for nanobind bindings

--- flake.nix
+++ flake.nix
@@ -3,13 +3,45 @@
 
   inputs.mc-rtc-nix.url = "github:mc-rtc/nixpkgs";
 
+  inputs.spacevecalg.url = "github:jrl-umi3218/SpaceVecAlg/pull/67/head";
+
   outputs =
     inputs:
     inputs.mc-rtc-nix.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
-        overrideAttrs.rbdyn = {
+        extraDevPyPackages = [ "rbdyn" ];
+
+        overlays = [
+          (pkgs-final: pkgs-prev: {
+            spacevecalg = inputs.spacevecalg.packages.${pkgs-final.system}.spacevecalg;
+          })
+        ];
+
+        overrideAttrs.rbdyn = 
+        { pkgs-final, drv-prev, ... }:
+        {
           src = lib.cleanSource ./.;
+          cmakeFlags = [
+            (lib.cmakeBool "PYTHON_BINDINGS" false)
+            (lib.cmakeBool "NANOBIND_BINDINGS" true)
+          ];
+          nativeBuildInputs = with pkgs-final; [
+            python3Packages.python
+          ] ++ drv-prev.nativeBuildInputs;
+          propagatedBuildInputs = with pkgs-final; [
+            python3Packages.nanoeigenpy
+            python3Packages.nanobind
+          ] ++ drv-prev.propagatedBuildInputs;
+        };
+
+        pyPackages = {
+          rbdyn =
+            {
+              pkgs,
+              toPythonModule,
+            }:
+            (toPythonModule (pkgs.rbdyn.override { }));
         };
       }
     );
